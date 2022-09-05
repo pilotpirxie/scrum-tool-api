0e2ecc489705f5e5333851d76158087354055e06
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2022-09-05 21:19:24 +0200

    Configure mysql db

--- src/models/db/dataSource.ts
+++ src/models/db/dataSource.ts
@@ -8,8 +8,12 @@ import Votes from './Votes';
 dotenv.config();
 
 const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: './db.sqlite',
+  type: 'mysql',
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  username: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   synchronize: true,
   logging: true,
   entities: [Boards, Cards, Users, Votes],
